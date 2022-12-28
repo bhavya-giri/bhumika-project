945c189daebb038aabce82893f35314bfab64ed9
Bhavya Giri <102273412+bhavya-giri@users.noreply.github.com>
2022-12-28 15:21:30 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from "react";
+import { BsImage } from "react-icons/bs";
+const App = () => {
   return (
-    <div className="App">
+    <div className="bg-black p-20 text-white h-screen w-screen">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className=" text-9xl">PICTURE +</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
 
-export default App
+      <div className="flex md:flex-row flex-col h-2/3 gap-10 mt-20">
+        <div className=" md:w-1/3 w-full">
+          <p className="text-3xl">Bringing your lost memories</p>
+          <div className="mt-10">
+            <p className="text-4xl">Instructions</p>
+            <ul className="text-2xl mt-5">
+              <li>1. Click on Drop/Upload image</li>
+              <li>2. Select an image from your device</li>
+              <li>3. Click on Upload and let the magic take place!</li>
+              <li>
+                4. You can preview the image and download it back to your device
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex gap-4 md:w-2/3 w-full">
+          <div className="flex flex-col justify-center items-center w-full border-dotted border-2 cursor-pointer rounded-lg">
+            <h1 className="text-5xl">+</h1>
+            <p>Drop or upload image</p>
+          </div>
+          <div className="flex flex-col justify-center items-center w-full border-dotted border-2 cursor-pointer rounded-lg">
+            <h1 className="text-5xl">
+              <BsImage />
+            </h1>
+            <p>Processed image</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
